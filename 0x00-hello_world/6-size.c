d2791d0a64a52d8b26d5fccd596d8ee229327142
ghithi <ossamaghithi8@gmail.com>
2023-07-13 17:52:45 -0700

    add c code number 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *Return: always 0
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int %d byte(s)", sizeof(lon int));
+	printf("Size of long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
