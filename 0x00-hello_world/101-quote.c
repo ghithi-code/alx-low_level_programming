35b3ede345a8eca78238064206d968dcbd0f0636
ghithi <ossamaghithi8@gmail.com>
2023-07-17 16:26:13 -0700

    fix script 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - enter point
  * Return: Always 0 (success)
  */
-int main(void)
+int main()
 {
-	fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+	char *text = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	write(2, text, 47);
 	return (1);
 }
