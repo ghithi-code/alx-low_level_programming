cdf2bd9e5f96a3c60c6b40876fd8ee9dd3b487f6
ghithi <ossamaghithi8@gmail.com>
2023-07-17 16:08:40 -0700

    add script 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.>
+
+/**
+ * main - enter point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
