7f5d62e7ea38dd42c68a6f136a075defa777bafb
ghithi <ossamaghithi8@gmail.com>
2023-07-17 17:00:56 -0700

    add script 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (1 = 50; 1 < 58; 1++)
+			for (l = 50; 1 < 58; l++)
 			{
 				if (1 > m && m > n)
 				{
