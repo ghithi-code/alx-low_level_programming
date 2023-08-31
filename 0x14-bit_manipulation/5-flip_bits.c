894cefa77e3433a84a5b7fc0822f2ec38ef6a85e
ghithi-code <ossamaghithi8@gmail.com>
2023-08-31 14:36:58 -0700

    add 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - convert  number to another number
+ * @n: first number
+ * @m: second number to convert
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int d;
+	int c;
+
+	d = n ^ m;
+	c = 0;
+
+	while (d)
+	{
+		c++;
+		d &= (d - 1);
+	}
+
+	return (c);
+}
+
+
