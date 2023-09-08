ab97b4b590b2371f8d0d44fac5db7b38f4a61ff0
ghithi-code <ossamaghithi8@gmail.com>
2023-09-08 14:31:33 -0700

    remove 3-cp.c and write it again :|

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,21 +6,22 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - allocates 1024 bytes for a buffer.
+ * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
- * Return: pointer to the newly-allocated buffer
+ * Return: A pointer to the newly-allocated buffer.
  * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
- * main - copie   file to another file.
- * @argc: The number of arguments to the program
- * @argv: array of pointers to the arguments.
- * Return:  0 = success.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success
  * Description: If the argument count is incorrect - exit code 97.
- * if file_from does not exist 98.
- * if file_to cannot be created exit code 99.
- * if file_to or file_from cannot closed - exit code 100
+ * if file_from does not exist or cannot be read - exit code 98.
+ * if file_to cannot be created or written to - exit code 99.
+ * if file_to or file_from cannot be closed - exit code 100.
  */
-
+ 
+ 
 char *create_buffer(char *file)
 {
 	char *buffer;
@@ -54,14 +55,12 @@ void close_file(int fd)
 
 int main(int argc, char *argv[])
 {
-
-
 	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "info: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -74,7 +73,7 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error:  Can't read from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
